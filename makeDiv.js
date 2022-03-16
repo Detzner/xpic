c0e684e99b5001cf80dba1325469d7700d85efac
RubelDetzner <RubelDetzner@vpnpool-10-250-46-154.near.illinois.edu>
2022-03-16 17:01:12 -0500

    Initial commit

---
+++ makeDiv.js
@@ -0,0 +1,25 @@
+// creates the text input box where you'll type your answer
+	function inputText() {
+		const input = document.createElement('input');
+		input.setAttribute('type', 'text');
+		input.setAttribute('size', '2');
+		return input;
+	}	
+// creates the button you click on to check your answer
+	function inputImage() {
+		const input = document.createElement('input');
+		input.setAttribute('type', 'image');
+		input.setAttribute('src', 'bluedot.jpg');	
+		input.setAttribute('onclick','checkInput(this)');
+		return input;
+	}
+// creates a div with a data row, data column, and 2 inputs		
+	function makeDiv(r, c) {
+		const div = document.createElement("div");
+		div.setAttribute("class", "dynamicDiv");
+		div.setAttribute("data-row", r);
+		div.setAttribute("data-column", c);
+		div.appendChild(inputText());
+		div.appendChild(inputImage());		
+		return div;
+	}		
---
+++ shuffleArray.js
@@ -0,0 +1,19 @@
+function shuffleArray(immutableArray) {
+	let copy = immutableArray.slice();
+	let currentIndex = copy.length,  randomIndex;
+
+	// While there remain elements to shuffle...
+	while (currentIndex != 0) {
+
+    	// Pick a remaining element...
+    	randomIndex = Math.floor(Math.random() * currentIndex);
+    	currentIndex--;
+
+    	// And swap it with the current element.
+    	[copy[currentIndex], copy[randomIndex]] = [
+      	copy[randomIndex], copy[currentIndex]];
+    }
+
+	return copy;
+  
+}
