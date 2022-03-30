5f68209fd022a8bded96ef3523a81f610ca4a7e2
Dick Detzner <rdetzner@illinois.edu>
2022-03-30 16:58:08 -0500

    reformatted makeDiv

--- makeDiv.js
+++ makeDiv.js
@@ -1,25 +1,29 @@
 // creates the text input box where you'll type your answer
-	function inputText() {
-		const input = document.createElement('input');
-		input.setAttribute('type', 'text');
-		input.setAttribute('size', '2');
-		return input;
-	}	
+function inputText () {
+	const input = document.createElement('input');
+	input.setAttribute('type', 'text');
+	input.setAttribute('size', '2');
+	return input;
+}
+	
 // creates the button you click on to check your answer
-	function inputImage() {
-		const input = document.createElement('input');
-		input.setAttribute('type', 'image');
-		input.setAttribute('src', 'images/bluedot.jpg');	
-		input.setAttribute('onclick','checkInput(this)');
-		return input;
-	}
-// creates a div with a data row, data column, and 2 inputs		
-	function makeDiv(r, c) {
-		const div = document.createElement("div");
-		div.setAttribute("class", "dynamicDiv");
-		div.setAttribute("data-row", r);
-		div.setAttribute("data-column", c);
-		div.appendChild(inputText());
-		div.appendChild(inputImage());		
-		return div;
-	}		
+function inputImage () {
+	const input = document.createElement('input');
+	input.setAttribute('type', 'image');
+	input.setAttribute('src', 'images/bluedot.jpg');	
+	input.setAttribute('onclick','checkInput(this)');
+	return input;
+}
+
+// creates a div with a data row, data column, and 2 inputs	. just a definition	
+function makeDiv (r, c) {
+	const div = document.createElement("div");
+	div.setAttribute("class", "dynamicDiv");
+	div.setAttribute("data-row", r);
+	div.setAttribute("data-column", c);
+	div.appendChild(inputText());
+	div.appendChild(inputImage());		
+	return div;
+}		
+
+export { makeDiv };
